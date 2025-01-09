17efd64835064cf6860f430f37be9ff6f6c0604a
DMarinhoCodacy <102817200+DMarinhoCodacy@users.noreply.github.com>
2025-01-09 15:42:23 +0000

    Update MathTest.java

--- src/test/java/com/codacy/utils/MathTest.java
+++ src/test/java/com/codacy/utils/MathTest.java
@@ -10,6 +10,7 @@ public class MathTest {
         Math math = new Math(23);
 
         assertEquals(7, math.magicAdd(3, 4));
+        assertEquals(7, math.magicAdd(4, 4));
     }
 
     // Uncomment this to have 100% coverage
